5b80a0e309fe94c350c3ceae399d5d5e7697c40b
Tung Nguyen <tungns.inf@gmail.com>
2018-07-30 11:52:17 +0800

    SecGroup name start with tf- prefix

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_db_subnet_group" "rds_public_subnet" {
 # Params group
 #--------------------
 resource "aws_db_parameter_group" "rds_params" {
-  name = "${var.rds_instance_name}-params"
+  name   = "${var.rds_instance_name}-params"
   family = "${var.rds_engine_version}"
 
   lifecycle {
@@ -45,15 +45,14 @@ resource "aws_db_parameter_group" "rds_params" {
   ## Need to handle a default params here for mysql, postgresl, etc
 }
 
-
 #--------------------
 # Security Group
 #--------------------
 resource "aws_security_group" "rds_sg" {
-  name        = "${var.rds_instance_name}-${var.rds_engine_name}-rds-sg"
+  name        = "tf-${var.rds_instance_name}-${var.rds_engine_name}-rds-sg"
   description = "${var.rds_instance_name} ${var.rds_engine_name} rds secgroup"
 
-  vpc_id      = "${var.vpc_id}"
+  vpc_id = "${var.vpc_id}"
 
   egress {
     from_port   = 0
@@ -72,94 +71,93 @@ resource "aws_security_group" "rds_sg" {
 }
 
 resource "aws_security_group_rule" "allow_connect_from_app" {
-  count           = "${length(compact(split(",", var.app_sg_ids)))}"
-  type            = "ingress"
-  from_port       = "${lookup(var.rds_ports, var.rds_engine_name)}"
-  to_port         = "${lookup(var.rds_ports, var.rds_engine_name)}"
-  protocol        = "tcp"
-  security_group_id         = "${aws_security_group.rds_sg.id}"
-  source_security_group_id  = "${element(split(",", var.app_sg_ids), count.index)}"
+  count                    = "${length(compact(split(",", var.app_sg_ids)))}"
+  type                     = "ingress"
+  from_port                = "${lookup(var.rds_ports, var.rds_engine_name)}"
+  to_port                  = "${lookup(var.rds_ports, var.rds_engine_name)}"
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.rds_sg.id}"
+  source_security_group_id = "${element(split(",", var.app_sg_ids), count.index)}"
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
-
 #-------------------------
 # Master Node
 #-------------------------
 resource "aws_db_instance" "rds_master" {
-  count                       = "${var.rds_multi_az ? 0 : 1}"
-  storage_type                = "${var.rds_storage_type}"
-  allocated_storage           = "${var.rds_storage_size}"
-  engine                      = "${var.rds_engine_name}"
-  engine_version              = "${var.rds_storage_engine_version}"
-  instance_class              = "${var.rds_instance_type}"
-  identifier                  = "${var.rds_instance_name}"
-  name                        = "${var.rds_instance_db_name}"
-  username                    = "${var.rds_instance_root_user_name}"
-  password                    = "${var.rds_instance_root_user_password}"
-  db_subnet_group_name        = "${var.rds_master_id == "" ? aws_db_subnet_group.rds_private_subnet.name : ""}"
-  parameter_group_name        = "${aws_db_parameter_group.rds_params.name}"
-  availability_zone           = "${element(split(",", var.azs), 0)}"
-  multi_az                    = false
-  publicly_accessible         = "${var.rds_publicly_accessible}"
-  vpc_security_group_ids      = ["${aws_security_group.rds_sg.id}"]
-  apply_immediately           = true
-  backup_retention_period     = "${var.rds_master_id == "" ? var.rds_backup_retention_period : 0}"
-  auto_minor_version_upgrade  = true
-  skip_final_snapshot         = "${var.rds_skip_final_snapshot}"
-  final_snapshot_identifier   = "${var.rds_instance_name}-final-snapshot"
-  monitoring_interval         = "${var.rds_monitoring_interval}"
-  monitoring_role_arn         = "${var.rds_monitoring_role_arn}"
-  copy_tags_to_snapshot       = "${var.copy_tags_to_snapshot}"
-  snapshot_identifier         = "${var.snapshot_identifier}"
+  count                      = "${var.rds_multi_az ? 0 : 1}"
+  storage_type               = "${var.rds_storage_type}"
+  allocated_storage          = "${var.rds_storage_size}"
+  engine                     = "${var.rds_engine_name}"
+  engine_version             = "${var.rds_storage_engine_version}"
+  instance_class             = "${var.rds_instance_type}"
+  identifier                 = "${var.rds_instance_name}"
+  name                       = "${var.rds_instance_db_name}"
+  username                   = "${var.rds_instance_root_user_name}"
+  password                   = "${var.rds_instance_root_user_password}"
+  db_subnet_group_name       = "${var.rds_master_id == "" ? aws_db_subnet_group.rds_private_subnet.name : ""}"
+  parameter_group_name       = "${aws_db_parameter_group.rds_params.name}"
+  availability_zone          = "${element(split(",", var.azs), 0)}"
+  multi_az                   = false
+  publicly_accessible        = "${var.rds_publicly_accessible}"
+  vpc_security_group_ids     = ["${aws_security_group.rds_sg.id}"]
+  apply_immediately          = true
+  backup_retention_period    = "${var.rds_master_id == "" ? var.rds_backup_retention_period : 0}"
+  auto_minor_version_upgrade = true
+  skip_final_snapshot        = "${var.rds_skip_final_snapshot}"
+  final_snapshot_identifier  = "${var.rds_instance_name}-final-snapshot"
+  monitoring_interval        = "${var.rds_monitoring_interval}"
+  monitoring_role_arn        = "${var.rds_monitoring_role_arn}"
+  copy_tags_to_snapshot      = "${var.copy_tags_to_snapshot}"
+  snapshot_identifier        = "${var.snapshot_identifier}"
 
   # Build a read replica from another RDS
-  replicate_source_db         = "${var.rds_master_id}"
+  replicate_source_db = "${var.rds_master_id}"
 
   lifecycle {
     create_before_destroy = true
   }
 
   tags {
-    Name        = "${var.rds_instance_name}",
-    Project     = "${var.project_name}",
-    Type        = "rds",
-    Layer       = "rds",
+    Name        = "${var.rds_instance_name}"
+    Project     = "${var.project_name}"
+    Type        = "rds"
+    Layer       = "rds"
     Environment = "${var.env}"
   }
 }
 
 resource "aws_db_instance" "rds_master_multi_az" {
-  count                       = "${var.rds_multi_az ? 1 : 0}"
-  storage_type                = "${var.rds_storage_type}"
-  allocated_storage           = "${var.rds_storage_size}"
-  engine                      = "${var.rds_engine_name}"
-  engine_version              = "${var.rds_storage_engine_version}"
-  instance_class              = "${var.rds_instance_type}"
-  identifier                  = "${var.rds_instance_name}"
-  name                        = "${var.rds_instance_db_name}"
-  username                    = "${var.rds_instance_root_user_name}"
-  password                    = "${var.rds_instance_root_user_password}"
-  db_subnet_group_name        = "${var.rds_master_id == "" ? aws_db_subnet_group.rds_private_subnet.name : ""}"
-  parameter_group_name        = "${aws_db_parameter_group.rds_params.name}"
-  multi_az                    = "${var.rds_multi_az}"
-  publicly_accessible         = "${var.rds_publicly_accessible}"
-  vpc_security_group_ids      = ["${aws_security_group.rds_sg.id}"]
-  apply_immediately           = true
-  backup_retention_period     = "${var.rds_master_id == "" ? var.rds_backup_retention_period : 0}"
-  auto_minor_version_upgrade  = true
-  skip_final_snapshot         = "${var.rds_skip_final_snapshot}"
-  final_snapshot_identifier   = "${var.rds_instance_name}-final-snapshot"
-  monitoring_interval         = "${var.rds_monitoring_interval}"
-  monitoring_role_arn         = "${var.rds_monitoring_role_arn}"
-  copy_tags_to_snapshot       = "${var.copy_tags_to_snapshot}"
-  snapshot_identifier         = "${var.snapshot_identifier}"
+  count                      = "${var.rds_multi_az ? 1 : 0}"
+  storage_type               = "${var.rds_storage_type}"
+  allocated_storage          = "${var.rds_storage_size}"
+  engine                     = "${var.rds_engine_name}"
+  engine_version             = "${var.rds_storage_engine_version}"
+  instance_class             = "${var.rds_instance_type}"
+  identifier                 = "${var.rds_instance_name}"
+  name                       = "${var.rds_instance_db_name}"
+  username                   = "${var.rds_instance_root_user_name}"
+  password                   = "${var.rds_instance_root_user_password}"
+  db_subnet_group_name       = "${var.rds_master_id == "" ? aws_db_subnet_group.rds_private_subnet.name : ""}"
+  parameter_group_name       = "${aws_db_parameter_group.rds_params.name}"
+  multi_az                   = "${var.rds_multi_az}"
+  publicly_accessible        = "${var.rds_publicly_accessible}"
+  vpc_security_group_ids     = ["${aws_security_group.rds_sg.id}"]
+  apply_immediately          = true
+  backup_retention_period    = "${var.rds_master_id == "" ? var.rds_backup_retention_period : 0}"
+  auto_minor_version_upgrade = true
+  skip_final_snapshot        = "${var.rds_skip_final_snapshot}"
+  final_snapshot_identifier  = "${var.rds_instance_name}-final-snapshot"
+  monitoring_interval        = "${var.rds_monitoring_interval}"
+  monitoring_role_arn        = "${var.rds_monitoring_role_arn}"
+  copy_tags_to_snapshot      = "${var.copy_tags_to_snapshot}"
+  snapshot_identifier        = "${var.snapshot_identifier}"
 
   # Build a read replica from another RDS
-  replicate_source_db         = "${var.rds_master_id}"
+  replicate_source_db = "${var.rds_master_id}"
 
   lifecycle {
     create_before_destroy = true
@@ -167,11 +165,10 @@ resource "aws_db_instance" "rds_master_multi_az" {
   }
 
   tags {
-    Name        = "${var.rds_instance_name}",
-    Project     = "${var.project_name}",
-    Type        = "rds",
-    Layer       = "rds",
+    Name        = "${var.rds_instance_name}"
+    Project     = "${var.project_name}"
+    Type        = "rds"
+    Layer       = "rds"
     Environment = "${var.env}"
   }
 }
-

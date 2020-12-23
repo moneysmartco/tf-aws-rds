d88005e93b53e180837eb89a1545e4c51b102049
apoorvshrivastava-ms <apoorv.shrivastava@moneysmart.com>
2020-12-23 09:11:30 +0530

    removing var

--- main.tf
+++ main.tf
@@ -185,7 +185,7 @@ resource "aws_db_instance" "rds_master_multi_az" {
   username                   = "${var.rds_instance_root_user_name}"
   password                   = "${var.rds_instance_root_user_password}"
   db_subnet_group_name       = "${var.rds_master_id == "" ? aws_db_subnet_group.rds_private_subnet.name : ""}"
-  parameter_group_name       = "${var.max_connections != ""  ? var.aws_db_parameter_group.rds_params_with_max_connections.name : var.aws_db_parameter_group.rds_params_without_max_connections.name }"
+  parameter_group_name       = "${var.max_connections != ""  ? aws_db_parameter_group.rds_params_with_max_connections.name : aws_db_parameter_group.rds_params_without_max_connections.name }"
   multi_az                   = "${var.rds_multi_az}"
   publicly_accessible        = "${var.rds_publicly_accessible}"
   vpc_security_group_ids     = ["${aws_security_group.rds_sg.id}"]

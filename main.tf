bc1c067ccb6f8fefc171ab30ac41a891b2f81261
alex arockiaraj <alex@francium.tech>
2021-02-22 11:41:36 +0530

    commented snapshot identifier

--- main.tf
+++ main.tf
@@ -156,10 +156,7 @@ resource "aws_db_instance" "rds_master" {
   monitoring_interval        = var.rds_monitoring_interval
   monitoring_role_arn        = var.rds_monitoring_role_arn
   copy_tags_to_snapshot      = var.copy_tags_to_snapshot
-  snapshot_identifier        = var.snapshot_identifier
-  lifecycle {
-    ignore_changes = ["snapshot_identifier"]
-  }
+  #snapshot_identifier        = var.snapshot_identifier
   deletion_protection        = var.deletion_protection
 
   # Build a read replica from another RDS
@@ -198,7 +195,7 @@ resource "aws_db_instance" "rds_master_multi_az" {
   monitoring_interval        = var.rds_monitoring_interval
   monitoring_role_arn        = var.rds_monitoring_role_arn
   copy_tags_to_snapshot      = var.copy_tags_to_snapshot
-  snapshot_identifier        = var.snapshot_identifier
+  #snapshot_identifier        = var.snapshot_identifier
 
   # Build a read replica from another RDS
   replicate_source_db = var.rds_master_id

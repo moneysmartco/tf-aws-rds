ce27b7f471dfe70bf245f4ba79e09d0712beb2b1
apoorvshrivastava-ms <apoorv.shrivastava@moneysmart.com>
2021-06-30 18:06:31 +0530

    setting count in variable

--- main.tf
+++ main.tf
@@ -144,7 +144,7 @@ resource "aws_db_instance" "rds_master" {
   username                   = var.rds_instance_root_user_name
   password                   = var.rds_instance_root_user_password
   db_subnet_group_name       = var.rds_master_id == "" ? aws_db_subnet_group.rds_private_subnet[0].name : ""
-  parameter_group_name       = var.custom_parameter_group_name == null ? aws_db_parameter_group.rds_params[count.index].name : var.custom_parameter_group_name
+  parameter_group_name       = var.custom_parameter_group_name == null ? aws_db_parameter_group.rds_params[0].name : var.custom_parameter_group_name
   availability_zone          = element(split(",", var.azs), 0)
   multi_az                   = false
   publicly_accessible        = var.rds_publicly_accessible
@@ -185,7 +185,7 @@ resource "aws_db_instance" "rds_master_multi_az" {
   username                   = var.rds_instance_root_user_name
   password                   = var.rds_instance_root_user_password
   db_subnet_group_name       = var.rds_master_id == "" ? aws_db_subnet_group.rds_private_subnet[0].name : ""
-  parameter_group_name       = var.custom_parameter_group_name == null ? aws_db_parameter_group.rds_params[count.index].name : var.custom_parameter_group_name
+  parameter_group_name       = var.custom_parameter_group_name == null ? aws_db_parameter_group.rds_params[0].name : var.custom_parameter_group_name
   multi_az                   = true
   publicly_accessible        = var.rds_publicly_accessible
   vpc_security_group_ids     = [aws_security_group.rds_sg.id]
